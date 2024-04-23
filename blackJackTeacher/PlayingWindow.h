4e46ff785a29cd7daf557f03fbcfa097cfa52eae
eastland-emma <emmaeastland@gmail.com>
2024-04-23 00:14:54 -0600

    Added documentation to the .h declarations of my methods

--- blackJackTeacher/PlayingWindow.h
+++ blackJackTeacher/PlayingWindow.h
@@ -20,6 +20,7 @@ public:
 signals:
     /// @brief tells the model that the hit button has been pressed and it needs to update the game.
     void hit();
+    /// @brief informs the model that the player is ready to proceed to the next line
     void nextLine();
 public slots:
 
@@ -36,18 +37,48 @@ public slots:
     void clearOldImages();
 
     void flipDealerCard(QImage);
+
+    ///
+    /// \brief messageRecieved slot triggered when a instruction message is recieved from the model. displays to the message box in the top left
+    /// \param message the message to display to the user
+    ///
     void messageRecieved(QString message);
+
     void winPopUp(bool);
     void lossPopUp(bool);
 
+    ///
+    /// \brief recievedLock slot triggered when a lock instruction is recieved. Locks all buttons but the one provided to the method
+    /// \param allBut the button that we do not want to lock
+    ///
     void recievedLock(QString allBut);
 
+    ///
+    /// \brief setReadingScript setter for the readingScript field
+    /// \param reading if we are reading from a script or not
+    ///
     void setReadingScript(bool reading);
 
+    ///
+    /// \brief unlockStand unlocks the next button and locks the other buttons
+    ///
     void unlockStand();
+
+    ///
+    /// \brief unlockHit unlocks the next button and locks the other buttons
+    ///
     void unlockHit();
+
+    ///
+    /// \brief unlockSplit unlocks the next button and locks the other buttons
+    ///
     void unlockSplit();
 
+    ///
+    /// \brief receivedProb updates the probability displayed to the user
+    /// \param probOfDealerBust
+    /// \param probOfDealerWin
+    ///
     void receivedProb(double probOfDealerBust, double probOfDealerWin);
 private slots:
     void on_nextButton_clicked();
